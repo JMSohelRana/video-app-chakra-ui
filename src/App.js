310edc5563319aff57b862f3cfa2a2af9d7751e3
Sohel <jmsohelrana04@gmail.com>
2023-06-26 11:45:12 +0600

    Carousel component added in home component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Header></Header>
       <Routes>
-        <Route></Route>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
       </Routes>
     </BrowserRouter>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,55 @@
+import { Box, Heading, Image } from '@chakra-ui/react';
 import React from 'react';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel';
+import img1 from '../assets/1.jpg';
+import img2 from '../assets/2.jpg';
+import img3 from '../assets/3.jpg';
+import img4 from '../assets/4.jpg';
+// import img5 from '../assets/5.png';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Box>
+      <MyCarousel></MyCarousel>
+    </Box>
+  );
 };
+
+const MyCarousel = () => (
+  <Carousel
+    autoPlay
+    infiniteLoop
+    showThumbs={false}
+    interval={1000}
+    showStatus={false}
+    showIndicators={false}
+  >
+    <Box w={'full'} h={'100vh'}>
+      <Image src={img1}></Image>
+      <Heading bgColor={'blackAlpha.600'} color={'white'}>
+        Watch The Future
+      </Heading>
+    </Box>
+    <Box w={'full'} h={'100vh'}>
+      <Image src={img2}></Image>
+      <Heading bgColor={'blackAlpha.600'} color={'white'}>
+        Watch The Future
+      </Heading>
+    </Box>
+    <Box w={'full'} h={'100vh'}>
+      <Image src={img3}></Image>
+      <Heading bgColor={'blackAlpha.600'} color={'white'}>
+        Watch The Future
+      </Heading>
+    </Box>
+    <Box w={'full'} h={'100vh'}>
+      <Image src={img4}></Image>
+      <Heading bgColor={'blackAlpha.600'} color={'white'}>
+        Watch The Future
+      </Heading>
+    </Box>
+  </Carousel>
+);
 
 export default Home;
