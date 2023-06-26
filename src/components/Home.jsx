7afeaa1172bc504e3c125714013ae4e70c8e6acc
Sohel <jmsohelrana04@gmail.com>
2023-06-26 13:15:11 +0600

    Carousel component added in home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { Box, Heading, Image } from '@chakra-ui/react';
+import { Box, Container, Heading, Image, Stack, Text } from '@chakra-ui/react';
 import React from 'react';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { Carousel } from 'react-responsive-carousel';
@@ -6,12 +6,67 @@ import img1 from '../assets/1.jpg';
 import img2 from '../assets/2.jpg';
 import img3 from '../assets/3.jpg';
 import img4 from '../assets/4.jpg';
-// import img5 from '../assets/5.png';
+import img5 from '../assets/5.png';
+
+const headingOptions = {
+  pos: 'absolute',
+  left: '50%',
+  top: '50%',
+  transform: 'translate(-50%,-50%)',
+  textTransform: 'uppercase',
+  p: 4,
+  size: '4xl',
+};
 
 const Home = () => {
   return (
     <Box>
       <MyCarousel></MyCarousel>
+      <Container minH={'100vh'} maxW={'container.xl'} p={'16'}>
+        <Heading
+          textTransform={'uppercase'}
+          py={2}
+          w={'fit-content'}
+          borderBottom={'2px solid'}
+          m={'auto'}
+        >
+          Services
+        </Heading>
+
+        <Stack
+          h={'full'}
+          p={'4'}
+          alignItems={'center'}
+          direction={['column', 'row']}
+        >
+          <Image
+            src={img5}
+            h={['40', '400']}
+            filter={'hue-rotate(-130deg)'}
+          ></Image>
+
+          <Text
+            letterSpacing={'widest'}
+            lineHeight={'190%'}
+            textAlign={'center'}
+            p={['4', '16']}
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+            eligendi consequatur tempora accusamus earum molestiae dolore.
+            Officiis illo eius eos ad quaerat, obcaecati voluptatibus vitae
+            distinctio! Obcaecati accusamus reiciendis esse doloribus
+            repudiandae reprehenderit beatae delectus sapiente illo,
+            repellendus, aliquam, nostrum incidunt rerum! Aperiam excepturi
+            facere eum magnam sunt sed officia quia repellat suscipit aliquid
+            minima deserunt nulla esse, laudantium totam, officiis est sapiente
+            voluptate alias quibusdam natus. Impedit quasi officiis, sapiente
+            doloribus, porro consequatur excepturi quas, debitis rerum
+            laudantium harum pariatur sequi eos quo. Eaque deleniti, cupiditate
+            magnam officiis voluptas, blanditiis libero omnis corrupti modi
+            vitae excepturi harum odit repellat?
+          </Text>
+        </Stack>
+      </Container>
     </Box>
   );
 };
@@ -27,26 +82,26 @@ const MyCarousel = () => (
   >
     <Box w={'full'} h={'100vh'}>
       <Image src={img1}></Image>
-      <Heading bgColor={'blackAlpha.600'} color={'white'}>
+      <Heading bgColor={'blackAlpha.600'} color={'white'} {...headingOptions}>
         Watch The Future
       </Heading>
     </Box>
     <Box w={'full'} h={'100vh'}>
       <Image src={img2}></Image>
-      <Heading bgColor={'blackAlpha.600'} color={'white'}>
-        Watch The Future
+      <Heading bgColor={'whiteAlpha.900'} color={'black'} {...headingOptions}>
+        Future Is Gaming
       </Heading>
     </Box>
     <Box w={'full'} h={'100vh'}>
       <Image src={img3}></Image>
-      <Heading bgColor={'blackAlpha.600'} color={'white'}>
-        Watch The Future
+      <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
+        Gaming On Console
       </Heading>
     </Box>
     <Box w={'full'} h={'100vh'}>
       <Image src={img4}></Image>
-      <Heading bgColor={'blackAlpha.600'} color={'white'}>
-        Watch The Future
+      <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
+        night life is cool
       </Heading>
     </Box>
   </Carousel>
